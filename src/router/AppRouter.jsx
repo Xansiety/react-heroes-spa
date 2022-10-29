61c98852b0a445923f39356256ac8c9e22912182
xansiety <ferando543@outlook.com>
2022-10-28 19:25:44 -0500

    Modulo:Heroes Se añaden nuevas rutas y componentes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,14 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { LoginPage  } from "../auth"
-import { DcPage, MarvelPage } from "../heroes" 
-import { Navbar } from "../ui"
+import { Route, Routes } from "react-router-dom"
+import { LoginPage  } from "../auth" 
+import { HeroesRoutes } from "../heroes" 
 
 export const AppRouter = () => {
   return (
-    <>  
-      <Navbar /> 
+    <>   
         <Routes>
-            <Route path="login" element={<LoginPage />} />
-            <Route path="marvel" element={<MarvelPage />} />
-            <Route path="dc" element={<DcPage />} />
+            <Route path="login" element={<LoginPage />} /> 
+            <Route path="/*" element={<HeroesRoutes />} /> 
 
-            <Route path="/" element={<Navigate to="/marvel" />} />
         </Routes>
     </>
   )
