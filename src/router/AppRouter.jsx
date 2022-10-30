5a65148b9196a25b0792701ecc06cb879acc5764
xansiety <ferando543@outlook.com>
2022-10-30 14:44:11 -0600

    Auth:PublicRoutes.jsx Se añaden rutas publicas y redireccionamiento si ya se tiene sesion iniciada

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes";
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
-
+        <Route
+          path="login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        ></Route>
+        
         <Route
           path="/*"
           element={
