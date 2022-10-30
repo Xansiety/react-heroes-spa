dfe558c49e5ac84f1d7d3899ea5610b29e3f5873
xansiety <ferando543@outlook.com>
2022-10-30 10:01:01 -0600

    Heroes:Component  Se añade componente de lista en heroes

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,23 +1,21 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
- 
 export const LoginPage = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const onLogin = () => {
-    navigate('/marvel',{
-      replace: true
-  })
-  }
+    navigate("/marvel", {
+      replace: true,
+    });
+  };
 
   return (
     <div className="container mt-5">
-      <h1>
-        Login
-      </h1>
+      <h1>Login</h1>
       <hr />
-      <button className="btn btn-primary" onClick={onLogin}>Login</button>
+      <button className="btn btn-primary" onClick={onLogin}>
+        Login
+      </button>
     </div>
-  )
-}
+  );
+};
--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,66 +1,65 @@
-import { Link, NavLink, useNavigate } from 'react-router-dom';
- 
+import { Link, NavLink, useNavigate } from "react-router-dom";
+
 export const Navbar = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate() 
+  const onLogout = () => {
+    navigate("/login", {
+      replace: true,
+    });
+  };
 
-    const onLogout = () => {
-        navigate('/login',{
-            replace: true
-        })
-    }
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
+      <Link className="navbar-brand" to="/">
+        Asociaciones
+      </Link>
 
-    return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
-            
-            <Link 
-                className="navbar-brand" 
-                to="/"
-            >
-                Asociaciones
-            </Link>
+      <div className="navbar-collapse">
+        <div className="navbar-nav">
+          <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? "active" : ""}`
+            }
+            to="/marvel"
+          >
+            Marvel
+          </NavLink>
 
-            <div className="navbar-collapse">
-                <div className="navbar-nav">
+          <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? "active" : ""}`
+            }
+            to="/dc"
+          >
+            DC
+          </NavLink>
 
-                    <NavLink  
-                        className={ ({isActive}) => `nav-item nav-link ${ isActive ? 'active' : '' }` }
-                        to="/marvel"
-                        
-                    >
-                        Marvel
-                    </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? "active" : ""}`
+            }
+            to="/search"
+          >
+            Search
+          </NavLink>
+        </div>
+      </div>
 
-                    <NavLink 
-                        className={ ({isActive}) => `nav-item nav-link ${ isActive ? 'active' : '' }` }
-                        to="/dc"
-                    >
-                        DC
-                    </NavLink>
-
-                    <NavLink 
-                        className={ ({isActive}) => `nav-item nav-link ${ isActive ? 'active' : '' }` }
-                        to="/search"
-                    >
-                        Search
-                    </NavLink>
-                </div>
-            </div>
-
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
-                <ul className="navbar-nav ml-auto">
-                    <span className='nav-item nav-link text-primary'>
-                        Fernando
-                    </span>
-                    <button className='nav-item nav-link btn' onClick={onLogout}>LogOut</button>
-                    {/* <NavLink 
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+        <ul className="navbar-nav ml-auto">
+          <span className="nav-item nav-link text-primary">Fernando</span>
+          <button className="nav-item nav-link btn" onClick={onLogout}>
+            LogOut
+          </button>
+          {/* <NavLink 
                         className={ ({isActive}) => `nav-item nav-link ${ isActive ? 'active' : '' }` }
                         to="/login"
                     >
                         Logout
                     </NavLink> */}
-                </ul>
-            </div>
-        </nav>
-    )
-}
+        </ul>
+      </div>
+    </nav>
+  );
+};
