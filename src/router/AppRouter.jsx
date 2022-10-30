d4aa91906f33ab1c908cb88b125fbc0b7722ceb6
xansiety <ferando543@outlook.com>
2022-10-30 14:33:22 -0600

    Auth:PrivateRoute.jsx Se añade proteccion de rutas privadas modulo heroes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,15 +1,23 @@
-import { Route, Routes } from "react-router-dom"
-import { LoginPage  } from "../auth" 
-import { HeroesRoutes } from "../heroes" 
+import { Route, Routes } from "react-router-dom";
+import { LoginPage } from "../auth";
+import { HeroesRoutes } from "../heroes";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
   return (
-    <>   
-        <Routes>
-            <Route path="login" element={<LoginPage />} /> 
-            <Route path="/*" element={<HeroesRoutes />} /> 
+    <>
+      <Routes>
+        <Route path="login" element={<LoginPage />} />
 
-        </Routes>
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          }
+        ></Route>
+      </Routes>
     </>
-  )
-}
+  );
+};
