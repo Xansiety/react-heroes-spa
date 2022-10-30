4d6f71a5d7762270eb37ba71f7cb9d711a8a3d29
xansiety <ferando543@outlook.com>
2022-10-30 11:37:25 -0600

    Heroes:Pages Integracion de Animate.css para crear animaciones con css

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -21,10 +21,10 @@ export const HeroPage = () => {
         <img
           src={`/assets/heroes/${hero.id}.jpg`}
           alt={hero.superhero}
-          className="img-thumbnail"
+          className="img-thumbnail animate__animated animate__fadeInLeft"
         />
       </div>
-      <div className="col-8">
+      <div className="col-8 animate__animated animate__bounceInRight">
         <h3>{hero.superhero}</h3>
         <ul className="list-group list-group-flush">
           <li className="list-group-item">
