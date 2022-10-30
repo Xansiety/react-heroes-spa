abdb4b43b8169262578843404c691f5798181352
xansiety <ferando543@outlook.com>
2022-10-30 09:14:27 -0600

    Routing:useNavigate Uso de customHook useNavigate (push/navigate)

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,6 +1,15 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
  
 export const Navbar = () => {
+
+    const navigate = useNavigate() 
+
+    const onLogout = () => {
+        navigate('/login',{
+            replace: true
+        })
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -43,7 +52,7 @@ export const Navbar = () => {
                     <span className='nav-item nav-link text-primary'>
                         Fernando
                     </span>
-                    <button className='nav-item nav-link btn'>LogOut</button>
+                    <button className='nav-item nav-link btn' onClick={onLogout}>LogOut</button>
                     {/* <NavLink 
                         className={ ({isActive}) => `nav-item nav-link ${ isActive ? 'active' : '' }` }
                         to="/login"
